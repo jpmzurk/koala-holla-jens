2017aaf2b233845e4aba8eb814b1782c4313657b
Karl Beck <karljohnbeck@gmail.com>
2020-08-06 13:35:21 -0500

    added router side post request infromation

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Router } = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
@@ -8,7 +9,19 @@ const koalaRouter = express.Router();
 
 
 // POST
-
+koalaRouter.post('/', (req, res) => {
+    let newBear = req.body;
+    console.log('adding bear', newBear);
+    let queryText = ` INSERT INTO "bears" ("name", "gender", "age", "ready_to_transfer","notes")
+     VALUES ($1, $2, $3, $4, $5);`
+    pool.query(queryText, [newBear.name, newBear.gender, newBear.age, newBear.readyForTransfer, newBear.notes])
+        .then((results) => {
+            res.sendStatus(201);
+        }).catch((error) => {
+            console.log('error in adding bear', error);
+            res.sendStatus(500);
+        })
+})
 
 // PUT
 
