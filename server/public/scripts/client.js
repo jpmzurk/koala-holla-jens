dac8a9bc2074704b00baba1c4bfef8807105b833
Karl Beck <karljohnbeck@gmail.com>
2020-08-06 12:04:56 -0500

    added client side post request

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,31 +10,38 @@ $( document ).ready( function(){
 }); // end doc ready
 
 function setupClickListeners() {
+  $('#getKoalas')
   $( '#addButton' ).on( 'click', function(){
     console.log( 'in addButton on click' );
-    // get user input and put in an object
-    // NOT WORKING YET :(
-    // using a test object
-    let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
-    };
-    // call saveKoala with the new obejct
-    saveKoala( koalaToSend );
+    saveKoala( );
   }); 
 }
 
 function getKoalas(){
   console.log( 'in getKoalas' );
-  // ajax call to server to get koalas
-  
+
 } // end getKoalas
 
 function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
   // ajax call to server to get koalas
- 
+   let koalaToSend = {
+    name: $('#nameIn').val(),
+    age: $('#ageIn').val(),
+    gender: $('#genderIn').val(),
+    readyForTransfer: $('#readyForTransferIn').val(),
+    notes: $('#notesIn').val()
+  };
+  // ajax call to server to get koalas
+$.ajax({
+  method: 'POST',
+  url: '/koalas',
+  data: koalaToSend
+}).then(function(response) {
+  console.log('the Response from server', response)
+  getKoalas()
+}).catch(function (error) {
+  console.log('error in POST', error)
+  alert('unable to add koala to server, please tray again later')
+})
 }
